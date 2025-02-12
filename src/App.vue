fe1b5fc9c71380b61ea4b72a012c255fc0966a88
JokerRen328 <=>
2025-02-13 06:17:48 +0800

    gonna add tailwind backing up just in case

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import TopBar from './components/TopBar.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
 
-  <RouterView />
+  <TopBar />
+
+
+  <!-- <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink> -->
+
+
+
+
 </template>
 
 <style scoped>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    middle section
+  </div>
+
+</template>
